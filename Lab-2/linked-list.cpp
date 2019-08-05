b3da990b3c3ef5a288c81c810824233ce15bbc5a
jainmohit2001 <45727354+jainmohit2001@users.noreply.github.com>
2019-08-05 16:30:43 +0530

    Add files via upload

---
+++ Lab-2/linked-list.cpp
@@ -0,0 +1,142 @@
+#include <bits/stdc++.h>
+#include <string>
+#include <vector>
+#define ll long long int
+using namespace std;
+
+struct point{
+	int x;
+	int y;
+	struct point* ptr;
+};
+
+void AddFirst(struct point **start,int a,int b){
+	struct point* temp = (struct point *)malloc(sizeof(struct point));
+	if(temp == NULL){
+		return;
+	}
+	temp->x = a;
+	temp->y = b;
+	temp->ptr = *start;
+	*start = temp;	
+}
+ int DelFirst(struct point **start){
+ 	if(*start == NULL){
+ 		return -1;
+ 	}
+ 	else{
+ 		struct point *temp = *start;
+ 		*start = (*start)->ptr;
+ 		free(temp);
+ 	}
+ 	return 0;
+ }
+
+ int Del(struct point **start,int x1, int y1){
+ 	struct point *temp = *start;
+ 	struct point *prev = NULL;
+ 	while(temp != NULL){
+ 		if(temp->x == x1 && temp->y == y1){
+ 			if(temp == *start){
+ 				int x=DelFirst(start);
+ 				return x;
+ 			}
+ 			prev->ptr = temp->ptr;
+ 			free(temp);
+ 			return 0;
+ 		}
+ 		else{
+ 			prev = temp;
+ 			temp = temp->ptr;
+ 		}
+ 	}
+ 	return -1;
+ }
+
+string searchd(struct point *start, int d){
+	string s = "";
+	int ctr=0;
+	struct point* temp = start;
+	while(temp != NULL){
+		float dist = ((float)temp->x*(float)temp->x) + ((float)temp->y*(float)temp->y);
+		if((float)d >= sqrt(dist)){
+			ctr++;
+			s.append("(");
+			s += to_string(temp->x);
+			s.append(",");
+			s += to_string(temp->y);
+			s.append(") ");
+		}
+		
+			temp = temp->ptr;
+		
+	}
+	if(ctr==0){
+		return "-1";
+	}
+return s;
+}
+string Search(struct point* start,int x1,int y1){
+	string s= "False";
+	struct point *temp = start;
+	while(temp != NULL){
+		if(temp->x == x1 && temp->y == y1){
+			s = "True";
+			return s;
+		}
+		else{
+			temp = temp->ptr;
+		}
+	}
+	return s;
+}
+
+int Length(struct point*start){
+ 	int len=0;
+ 	struct point*temp = start;
+ 	while(temp != NULL){
+ 		len++;
+ 		temp = temp->ptr;
+ 	}
+ 	return len;
+}
+void print(struct point *start){
+	struct point *temp = start;
+	while(temp != NULL){
+		cout<<"("<<temp->x<<","<<temp->y<<") ";
+		temp = temp->ptr;
+	}
+	cout<<endl;
+}
+
+
+int main(){
+	struct point *head = NULL;
+	int n;
+	cin>>n;
+	int arr[n][3];
+	for(int i=0;i<n;i++){
+		cin>>arr[i][0];
+		switch (arr[i][0]){
+			case 1: cin>>arr[i][1]>>arr[i][2];break;
+			case 3: cin>>arr[i][1]>>arr[i][2];break;
+			case 5: cin>>arr[i][1]>>arr[i][2];break;
+			case 2: break;
+			case 6: break;
+			case 4: cin>>arr[i][1];break;
+		}
+	}
+	cout<<endl;
+	for(int i=0;i<n;i++){
+
+		switch (arr[i][0]){
+			case 1: AddFirst(&head,arr[i][1],arr[i][2]);break;
+			case 2:	cout<<DelFirst(&head)<<endl;break;
+			case 3: cout<<Del(&head,arr[i][1],arr[i][2])<<endl;break;
+			case 4:	cout<<searchd(head,arr[i][1])<<endl;break;
+			case 5: cout<<Search(head,arr[i][1],arr[i][2])<<endl;break;
+			case 6: cout<<Length(head)<<endl;break;
+		}
+	}
+	return 0;
+}
